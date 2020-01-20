5781b9bde1c461e9590e41abf09763fa0a3b09cd
Bradley Jensen <bradley13@gmail.com>
2020-01-20 16:13:24 -0500

    created a postDataRoute

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+var postDataRouter = require("./POST/postDataRouter");
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 router.get("/", (req, res) => {
@@ -10,5 +12,7 @@ router.get("/", (req, res) => {
   );
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
+
+router.use(postDataRouter);
 
 module.exports = router;
